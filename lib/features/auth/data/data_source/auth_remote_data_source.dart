cf7a1f9fe2c32bb69c79a1b59610551edc5c8de6
bipinaghimire <bipinaghimire6@gmail.com>
2023-06-20 12:13:35 +0545

    auth data

--- lib/features/auth/data/data_source/auth_remote_data_source.dart
+++ lib/features/auth/data/data_source/auth_remote_data_source.dart
@@ -6,6 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:student_clean_arch/config/constants/api_endpoint.dart';
 import 'package:student_clean_arch/core/failure/failure.dart';
 import 'package:student_clean_arch/core/network/remote/http_service.dart';
+import 'package:student_clean_arch/features/auth/domain/entity/student_entity.dart';
 
 final authRemoteDataSourceProvider = Provider(
   (ref) => AuthRemoteDataSource(dio: ref.read(httpServiceProvider)),
@@ -15,6 +16,41 @@ class AuthRemoteDataSource {
   final Dio dio;
 
   AuthRemoteDataSource({required this.dio});
+
+  Future<Either<Failure, bool>> registerStudent(StudentEntity student) async {
+    try {
+      Response response = await dio.post(
+        ApiEndpoints.register,
+        data: {
+          "fname": student.fname,
+          "lname": student.lname,
+          "image": student.image,
+          "username": student.username,
+          "password": student.password,
+          "batch": student.batch!.batchId,
+          // "course": ["6489a5908dbc6d39719ec19c", "6489a5968dbc6d39719ec19e"]
+          "course": student.courses.map((e) => e.courseId).toList(),
+        },
+      );
+      if (response.statusCode == 200) {
+        return const Right(true);
+      } else {
+        return Left(
+          Failure(
+            error: response.data["message"],
+            statusCode: response.statusCode.toString(),
+          ),
+        );
+      }
+    } on DioError catch (e) {
+      return Left(
+        Failure(
+          error: e.error.toString(),
+          statusCode: e.response?.statusCode.toString() ?? '0',
+        ),
+      );
+    }
+  }
 
   // uploadusing multipart
   Future<Either<Failure, String>> uploadProfilePicture(
